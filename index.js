434f1587d1ad072eb62ce6066cec95e71f2e1fff
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-05 13:59:39 +0200

    Added stripeRouter and path into index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const stripeRoute = require('./routes/stripe');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "views")));
 app.use(express.json());
+
+app.use('/stripe', stripeRoute)
